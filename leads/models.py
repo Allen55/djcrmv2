c14ac2050507f79cadcf90cae54905a5299ffe90
Allen Harper <aharper1609@gmail.com>
2021-05-01 13:31:53 -0700

    forms added

--- leads/models.py
+++ leads/models.py
@@ -9,7 +9,7 @@ class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
-    client = models.ForeignKey("Agent", on_delete=models.CASCADE)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, Agent
+from .forms import LeadForm, LeadModelForm
 
 def lead_list(request):
     leads = Lead.objects.all()
@@ -17,4 +18,26 @@ def lead_detail(request, pk):
     return render(request, 'leads/lead_detail.html', context)
 
 def lead_create(request):
-    return render(request, 'leads/lead_create.html')
+    form = LeadModelForm()
+    if request.method == "POST":
+        print('receiving a post request')
+        form = LeadModelForm(request.POST)
+        if form.is_valid():
+            print('form is valid')
+            print(form.cleaned_data)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                age=age,
+                agent=agent
+            )
+            print("Lead has been created")
+            return redirect("/leads")
+    context = {
+        "form": LeadForm()
+    }
+    return render(request, 'leads/lead_create.html', context)
